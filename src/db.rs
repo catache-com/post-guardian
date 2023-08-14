bc7647aad7dbcbbf647222d7cd1e3d1f67d4e832
stephen <stephen.ma@xcreators.com>
2023-08-14 21:30:48 +1000

    INIT: PostgresHandler.

---
+++ src/db.rs
@@ -0,0 +1,32 @@
+use crate::models;
+use anyhow::Result;
+use deadpool_postgres::{Manager, ManagerConfig, Pool};
+use models::PostgresDatabaseConfig;
+use tokio_postgres::{Config, NoTls};
+
+pub async fn init_postgres_connection(postgres_config: &PostgresDatabaseConfig) -> Result<Pool> {
+    let mut pg_config = Config::new();
+    pg_config
+        .user(&postgres_config.postgres_db_username)
+        .password(&postgres_config.postgres_db_password)
+        .host(&postgres_config.postgres_db_host)
+        .port(postgres_config.postgres_db_port)
+        .dbname(&postgres_config.postgres_db_name);
+
+    let manager = Manager::from_config(pg_config, NoTls, ManagerConfig::default());
+    let pool = Pool::builder(manager)
+        .max_size(postgres_config.postgres_db_max_connections)
+        .build()?;
+    Ok(pool)
+}
+
+pub struct PostgresHandler {
+    pool: Pool,
+}
+
+impl PostgresHandler {
+    pub async fn new(postgres_config: &PostgresDatabaseConfig) -> Result<Self> {
+        let pool = init_postgres_connection(postgres_config).await?;
+        Ok(PostgresHandler { pool })
+    }
+}
