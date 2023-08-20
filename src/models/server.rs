4cbef5161d3858cc4e0e88edb72478302250e30b
stephen <u6269649@anu.edu.au>
2023-08-20 14:44:02 +1000

    BUILD & ENH: ServerError.

--- src/models/server.rs
+++ src/models/server.rs
@@ -1,7 +1,50 @@
-use axum::http::StatusCode;
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
+use serde_json::json;
 
 #[derive(Debug)]
 pub struct ServerError {
     pub message: Option<String>,
     pub status_code: StatusCode,
+}
+
+impl ServerError {
+    pub fn new(status_code: StatusCode) -> ServerError {
+        ServerError {
+            message: None,
+            status_code,
+        }
+    }
+    pub fn new_with_message(message: impl Into<String>, status_code: StatusCode) -> ServerError {
+        ServerError {
+            message: Some(message.into()),
+            status_code,
+        }
+    }
+
+    pub fn internal_server_error() -> ServerError {
+        ServerError {
+            message: None,
+            status_code: StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+impl IntoResponse for ServerError {
+    fn into_response(self) -> Response {
+        if self.message.is_some() {
+            (
+                self.status_code,
+                Json(json!({
+                  "error": self.message
+                })),
+            )
+                .into_response()
+        } else {
+            self.status_code.into_response()
+        }
+    }
 }
