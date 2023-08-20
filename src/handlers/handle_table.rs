d7dedcab6fa5ee89e17345762a2bd3931c9da4ec
stephen <u6269649@anu.edu.au>
2023-08-20 14:18:39 +1000

    DESIGN

--- src/handlers/handle_table.rs
+++ src/handlers/handle_table.rs
@@ -5,3 +5,23 @@ use tracing::instrument;
 pub async fn new_table() -> Result<(), ServerError> {
     Ok(())
 }
+
+#[instrument(level = "trace", skip_all)]
+pub async fn table_info() -> Result<(), ServerError> {
+    Ok(())
+}
+
+#[instrument(level = "trace", skip_all)]
+pub async fn list_tables() -> Result<(), ServerError> {
+    Ok(())
+}
+
+#[instrument(level = "trace", skip_all)]
+pub async fn update_table_schema() -> Result<(), ServerError> {
+    Ok(())
+}
+
+#[instrument(level = "trace", skip_all)]
+pub async fn drop_table() -> Result<(), ServerError> {
+    Ok(())
+}
